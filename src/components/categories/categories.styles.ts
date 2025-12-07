7f862c44f2a1ba3c83f0b7099123ef713beb5b2f
DoniDevRs <doni.devrs@gmail.com>
2025-12-07 19:07:00 -0300

    style: add responsiveness to Categories e Catogory-item component.

--- src/components/categories/categories.styles.ts
+++ src/components/categories/categories.styles.ts
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const CategoriesContainer = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
   justify-content: center;
-`;
+`
 
 export const CategoriesContent = styled.div`
   height: 100%;
@@ -18,19 +18,48 @@ export const CategoriesContent = styled.div`
   grid-gap: 15px;
   padding: 30px;
 
-  & > div:nth-child(1) {
+  & div:nth-child(1) {
     grid-area: a;
   }
-  & > div:nth-child(2) {
+
+  & div:nth-child(2) {
     grid-area: b;
   }
-  & > div:nth-child(3) {
+
+  & div:nth-child(3) {
     grid-area: c;
   }
-  & > div:nth-child(4) {
+
+  & div:nth-child(4) {
     grid-area: d;
   }
-  & > div:nth-child(5) {
+
+  & div:nth-child(5) {
     grid-area: e;
   }
-`;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+
+    & div:nth-child(1) {
+      flex: 1;
+    }
+
+    & div:nth-child(2) {
+      flex: 1;
+    }
+
+    & div:nth-child(3) {
+      flex: 1;
+    }
+
+    & div:nth-child(4) {
+      flex: 1;
+    }
+
+    & div:nth-child(5) {
+      flex: 1;
+    }
+  }
+`
