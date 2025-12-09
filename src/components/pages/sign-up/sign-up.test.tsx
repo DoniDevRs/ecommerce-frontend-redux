756653cb77704f7dd4792151cd67fc5394414e94
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-09 15:38:06 -0300

    test: ensure sign up shows error when trying to submit without filling required fields.

---
+++ src/components/pages/sign-up/sign-up.test.tsx
@@ -0,0 +1,22 @@
+import userEvent from '@testing-library/user-event'
+import { renderWithRedux } from '../../helpers/test.helpers'
+import SignUpPage from './sign-up.page'
+import { screen } from '@testing-library/react'
+
+jest.mock('firebase/auth')
+
+describe('Sign Up', () => {
+  it('should show error when trying to submit without filling all required fields', async () => {
+    renderWithRedux(<SignUpPage />, {})
+
+    const submitButton = screen.getByText('Create Account', { selector: 'button' })
+
+    userEvent.click(submitButton)
+
+    await screen.findByText(/first name is required/i)
+    screen.getByText(/last name is required/i)
+    screen.getByText(/email is required/i)
+    screen.getByText(/password is required/i)
+    screen.getByText(/password confirmation is required/i)
+  })
+})
