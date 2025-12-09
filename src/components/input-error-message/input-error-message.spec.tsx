270c1821ed79d822c6817aabea443475ea0d5f57
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-09 08:37:53 -0300

    test: ensure input error message shows red error message.

---
+++ src/components/input-error-message/input-error-message.spec.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom';
+import Colors from '../../theme/theme.colors'
+import InputErrorMessage from './input-error-message.component'
+
+describe('Input Error Message', () => {
+  it('should show message with error color', () => {
+    render(<InputErrorMessage>Lorem Ipsum</InputErrorMessage>)
+
+    const message = screen.getByText('Lorem Ipsum')
+
+    expect(message).toHaveStyle({ color: Colors.error })
+  })
+})
