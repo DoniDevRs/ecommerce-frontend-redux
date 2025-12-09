89dad801e26427cfe37147d2540166809ac637b2
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-09 08:17:42 -0300

    test: ensure custom input renders with error if hasError is true.

---
+++ src/components/custom-input/custom-input.spec.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Colors from '../../theme/theme.colors'
+import CustomInput from './custom-input.component'
+
+describe('Custom Input', () => {
+  it('should render with error if hasError is true', () => {
+    render(<CustomInput placeholder="lorem ipsum" hasError={true} />)
+    const input = screen.getByPlaceholderText('lorem ipsum')
+    expect(input).toHaveStyle({ border: `2px solid ${Colors.error}` })
+  })
+})
